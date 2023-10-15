04e3cc0f30fafc0666f270ea8fd89d616cbc5541
Victoria <victoria.j.art@gmail.com>
2023-10-14 23:45:10 -0700

    finished up mobile navigation

--- app/_components/logo.js
+++ app/_components/logo.js
@@ -2,9 +2,11 @@ import Image from 'next/image';
 import '..//_styles/components/logo.styles.scss';
 
 
-export default function Logo () {
+export default function Logo ({ toggleColor }) {
     return(
-        <div className="logo">
+        <div className={`logo
+            ${toggleColor ? 'logo__white' : ''}
+        `}>
             <div className="logo__top">
                 salt of the earth
             </div>
