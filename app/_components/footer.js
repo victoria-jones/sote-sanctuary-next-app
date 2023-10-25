7dbb6367294155cc13b2c3e79c3ff863ff6df206
Victoria <victoria.j.art@gmail.com>
2023-10-25 13:45:05 -0700

    styled the footer

--- app/_components/footer.js
+++ app/_components/footer.js
@@ -1,12 +1,17 @@
+import Logo from './logo';
+import '..//_styles/components/footer.styles.scss';
+
 export default function Footer () {
     const year = new Date().getFullYear();
 
     return(
-        <footer>
-            <div>Logo</div>
-            <section>
-                <p>a non-profit farm and sanctuary for all</p>
-                <p>© {year}</p>
+        <footer className="footer">
+            <div className="footer__logo">
+                <Logo toggleColor />
+            </div>
+            <section className="footer__text">
+                <p className="footer__text__paragraph">a non-profit farm and sanctuary for all</p>
+                <p className="footer__text__paragraph">© {year}</p>
             </section>
         </footer>
     );
