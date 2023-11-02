16f5d68cb33e4adad9e0da24911848d59a76a841
Victoria <victoria.j.art@gmail.com>
2023-11-02 13:05:15 -0700

    trying edit to viewport size component

--- app/_components/viewport-size.js
+++ app/_components/viewport-size.js
@@ -1,12 +1,15 @@
 'use client';
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 //Component for detecting viewport size
     //used for components that need to be mounted/dismounted based on viewport size
 export default function ViewportSize () {
     //intial variables
-    const [ width, setWidth ] = useState(window.innerWidth);
-    const [ height, setHeight ] = useState(window.innerHeight);
+
+    //const [ width, setWidth ] = useState(window.innerWidth);
+    //const [ height, setHeight ] = useState(window.innerHeight);
+    const [ width, setWidth ] = useState(null);
+    const [ height, setHeight ] = useState(null);
 
     //watch for resizing and reset the window size
     useEffect(() => {
