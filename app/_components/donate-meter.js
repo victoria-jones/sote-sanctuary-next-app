347c20028b4f425661165484e0c726529ef31280
Victoria <victoria.j.art@gmail.com>
2023-12-08 11:43:09 -0700

    no synchronous scripts error
    donorbox add ins were not included properly and I'm fixing that now. errors when scripts are not loaded async

--- app/_components/donate-meter.js
+++ app/_components/donate-meter.js
@@ -1,18 +1,47 @@
+'use client';
+import { useEffect } from 'react';
 import '..//_styles/components/donate-meter.styles.scss';
 
 export default function DonateMeter ({ addClass }) {
+
+    useEffect(()=> {
+        const loadScript = async () => {
+            try{
+                const script = document.createElement('script');
+                script.type = 'text/javascript';
+                script.async = true;
+                script.src = 'https://donorbox.org/widget.js'
+                script.paypalExpress= false;
+
+                document.head.appendChild(script);
+            } catch (error) {
+                console.error('Error loading script:', error);
+            }
+        }
+
+        loadScript();
+
+        //remove script when/if component is unmounted
+            //prevent memory leaks
+        return () => {
+            const scriptElement = document.querySelector(
+                'script[scr="https://donorbox.org/widget.js"]'
+            );
+
+            if (scriptElement) {
+                document.head.removeChild(scriptElement);
+            }
+        }
+    },[]);
+
     return(
         <div className={`
             ${addClass ? addClass : ''}
             donate-meter
         `}>
-            <script
-                src="https://donorbox.org/widget.js"
-                paypalExpress="false">
-            </script>
             <iframe
                 className="donate-metera__iframe"
-                frameborder="0"
+                frameBorder="0"
                 height="93px"
                 name="donorbox"
                 scrolling="no"
