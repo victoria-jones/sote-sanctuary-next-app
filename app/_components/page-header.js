cb86e2f68ca5f39a1dbd865c7ce99cb13060ac10
Victoria <victoria.j.art@gmail.com>
2024-02-02 15:28:56 -0700

    styling about bio area

--- app/_components/page-header.js
+++ app/_components/page-header.js
@@ -1,10 +1,12 @@
+import '..//_styles/components/page-header.styles.scss';
 import FeaturedBanner from '..//_components/featured-banner.js';
+import StyledLine from '..//_components/styled-line';
 
 export default function PageHeader ({ children, title, subtitleTopText,subtitleBottomText, image, video }) {
     return(
         <section className="page-header">
             <FeaturedBanner />
-            <section className="page-header__wrapper">
+            <section className="page-header__wrapper section-margin">
 
                 <div className="page-header__image">
                     image holder
@@ -12,17 +14,22 @@ export default function PageHeader ({ children, title, subtitleTopText,subtitleB
 
                 <div className="page-header__content">
 
-                    <h2 className="page-header__title">
-                        { title }
-                    </h2>
+                    <div className="page-header__title--wrapper">
+                        <h2 className="page-header__title">
+                            { title }
+                        </h2>
 
-                    <div className="page-header__subtitle__wrapper">
-                        <h3 className="page-header__subtitle">
-                            { subtitleTopText } <br />
-                            <span className="page-header__subtitle__bottomText">
-                                { subtitleBottomText }
-                            </span>
-                        </h3>
+
+                        <div className="page-header__subtitle__wrapper">
+                            <h3 className="page-header__subtitle">
+                                { subtitleTopText } <br />
+                                <span className="page-header__subtitle__bottomtext">
+                                    { subtitleBottomText }
+                                </span>
+                            </h3>
+                        </div>
+
+                        <StyledLine />
                     </div>
 
                     <div className="page-header__bio">
---
+++ app/_components/styled-line.js
@@ -0,0 +1,7 @@
+import '..//_styles/components/styled-line.styles.scss';
+
+export default function StyledLine () {
+    return(
+        <div className="styled-line"></div>
+    );
+}
